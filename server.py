7e3ab672abb72637216f12fa720ce76442850ed9
minecreter <88221143+minecreter@users.noreply.github.com>
2021-12-19 10:27:33 +0900

    Update server.py

--- server.py
+++ server.py
@@ -31,5 +31,5 @@ def watch(name=None):
         json.dump(db, f)
 
     return render_template('watch.html', video=video)
-
-app.run(host="0.0.0.0",port="5000")
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0",port=port)
